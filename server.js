ce1f4c944ec851e2e093a237b2e8f65e81bfd931
studiant7 <hwawimobile123@gmail.com>
2025-04-03 12:37:25 +0100

    enable cors

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ require('dotenv').config();
 const express = require('express');
 const userRoutes = require('./src/routes/userRoutes');
 const db = require('./src/config/db'); 
+const cors = require("cors");
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
+
 app.use("/user", userRoutes);
 
 app.listen(PORT, () => {
